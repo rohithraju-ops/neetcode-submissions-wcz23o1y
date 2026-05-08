1342946f0678581115743370d15f0285c8d1f3c7
Rohith Raju <rohithrajuwork@gmail.com>
2026-05-08 12:00:31 -0700

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,8 @@
+from collections import Counter
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        counts = Counter(nums)
+        for num, count in counts.items():
+            if count == 1:
+                return num
+        
